65eb002385c16a8e31bca464ae9c0b24e3d44e2b
SaskiiaLc <52450052+SaskiiaLc@users.noreply.github.com>
2019-07-15 21:04:02 +0200

    Add files via upload

---
+++ 00_hello.rb
@@ -0,0 +1,13 @@
+def hello
+    return "Hello!"
+end
+
+
+def greet(name)
+    return "Hello, #{name}!"
+    
+end
+
+
+
+
---
+++ 03_basics.rb
@@ -0,0 +1,6 @@
+def reverse_upcase_noLTA (toto)
+    def reverse(string)
+    end
+end
+
+reverse_upcase_noLTA("Tries this at Home, Kids")
